9c9bbd0ff48d23d15fe32edd9fef7841ac087dbe
Remi Gau <remi_gau@hotmail.com>
2018-03-15 11:41:29 +0100

    More plotting sub-functions

---
+++ subfun/plot/ColorSubject.m
@@ -0,0 +1,21 @@
+function COLOR_Subject = ColorSubject()
+
+% Color for Subjects
+COLOR_Subject= [
+    0,0,0;
+    31,120,180;
+    178,223,138;
+%     51,160,44;
+    251,154,153;
+    227,26,28;
+    253,191,111;
+    255,127,0;
+    202,178,214;
+    106,61,154;
+%     0,0,130;
+    177,89,40;
+    125,125,125];
+COLOR_Subject=COLOR_Subject/255;
+
+end
+
