ae21633bb90c727c2ad7b37c3adb79ed83005026
Remi Gau <remi_gau@hotmail.com>
2020-11-03 07:03:32 +0100

    adapt set dir to new folder structure

--- src/subfun/set_dir.m
+++ src/subfun/set_dir.m
@@ -1,21 +1,39 @@
-function [Dirs] = set_dir()
+function [Dirs, SubLs, NbSub] = set_dir()
+    % Function used to define where the code and the data are
+    %
 
     if isunix
-        CodeDir = '/home/remi/github/AVT_analysis';
         StartDir = '/home/remi';
     elseif ispc
-        CodeDir = 'D:\github\AVT-7T-code';
         StartDir = 'D:\';
     else
         disp('Platform not supported');
     end
 
-    Dirs.CodeDir = CodeDir;
-    Dirs.CodeDir = StartDir;
-
+    % Derivatives Data
     Dirs.DerDir = fullfile(StartDir, 'Dropbox', 'PhD', 'Experiments', 'AVT', 'derivatives');
+    if ~exist(Dirs.DerDir, 'dir')
+        error('The data directory does not exist: %s', Dirs.DerDir);
+    end
+
+    %% DO NOT TOUCH
+    
+    Dirs.StartDir = StartDir;
+    
+    Dirs.CodeDir = fullfile(fileparts(mfilename('fullpath')), '..');
 
     Dirs.FigureFolder = fullfile(Dirs.DerDir, 'figures');
 
     Dirs.MVPA_resultsDir = fullfile(Dirs.DerDir, 'results', 'SVM');
     Dirs.BOLD_resultsDir = fullfile(Dirs.DerDir, 'results', 'profiles', 'surf');
+    
+    [~, ~, ~] = mkdir(Dirs.FigureFolder);
+    [~, ~, ~] = mkdir(Dirs.MVPA_resultsDir);
+    [~, ~, ~] = mkdir(Dirs.BOLD_resultsDir);
+    
+    SubLs = dir(fullfile(Dirs.DerDir, 'sub*'));
+    NbSub = numel(SubLs);
+    
+    Get_dependencies(Dirs);
+
+end
