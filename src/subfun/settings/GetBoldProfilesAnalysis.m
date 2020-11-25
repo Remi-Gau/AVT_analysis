3398f9bf477d2fa910e9013e6a60d6e34326f667
Remi Gau <remi_gau@hotmail.com>
2020-11-25 12:50:38 +0100

    reorganize BOLD profile plotting part 1

--- src/subfun/settings/GetBoldProfilesAnalysis.m
+++ src/subfun/settings/GetBoldProfilesAnalysis.m
@@ -1,54 +1,54 @@
 function Analysis = GetBoldProfilesAnalysis(opt)
 
-  %% Baseline Ipsi
+    %% Baseline Ipsi
     Analysis(1)      = struct('name', 'Audio:Ipsi', ...
-                        'conditions', 1);
+                              'conditions', 1);
 
     Analysis(end + 1) = struct('name', 'Visual:Ipsi', ...
-                          'conditions', 3);
+                               'conditions', 3);
 
     Analysis(end + 1) = struct('name', 'Tactile:Ipsi', ...
-                          'conditions', 5);
-                        
-  %% Baseline Contra
+                               'conditions', 5);
+
+    %% Baseline Contra
     Analysis(end + 1) = struct('name', 'Audio:Contra', ...
-                        'conditions', 1);
+                               'conditions', 1);
 
     Analysis(end + 1) = struct('name', 'Visual:Contra', ...
-                          'conditions', 3);
+                               'conditions', 3);
 
     Analysis(end + 1) = struct('name', 'Tactile:Contra', ...
-                          'conditions', 5);
-  
+                               'conditions', 5);
+
     %% Contra - Ipsi
     Analysis(end + 1) = struct('name', 'Audio:Contra-Ipsi', ...
-                        'conditions', [2 1]);
+                               'conditions', [2 1]);
 
     Analysis(end + 1) = struct('name', 'Visual:Contra-Ipsi', ...
-                          'conditions', [4 3]);
+                               'conditions', [4 3]);
 
     Analysis(end + 1) = struct('name', 'Tactile:Contra-Ipsi', ...
-                          'conditions', [6 5]);
+                               'conditions', [6 5]);
 
     %% Cross senses Ipsi
     Analysis(end + 1) = struct('name', 'Ipsi:Audio-Visual', ...
-                          'conditions', [1 3]);
+                               'conditions', [1 3]);
 
     Analysis(end + 1) = struct('name', 'Ipsi:Audio-Tactile', ...
-                          'conditions', [1 5]);
+                               'conditions', [1 5]);
 
     Analysis(end + 1) = struct('name', 'Ipsi:Visual-Tactile', ...
-                          'conditions', [3 5]);
+                               'conditions', [3 5]);
 
     %% Cross senses Contra
     Analysis(end + 1) = struct('name', 'Contra:Audio-Visual', ...
-                          'conditions', [2 4]);
+                               'conditions', [2 4]);
 
     Analysis(end + 1) = struct('name', 'Contra:Audio-Tactile', ...
-                          'conditions', [2 6]);
+                               'conditions', [2 6]);
 
     Analysis(end + 1) = struct('name', 'Contra:Visual-Tactile', ...
-                          'conditions', [4 6]);
+                               'conditions', [4 6]);
 
     if opt.verbose
         fprintf('\n\n');
