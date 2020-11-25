975b68c405233140c90eae3a75b63a05b2749d24
Remi Gau <remi_gau@hotmail.com>
2020-11-25 11:41:32 +0100

    reorganize PCM plotting part 1

--- src/figures/SubjectColorLegend.m
+++ src/figures/SubjectColorLegend.m
@@ -1,6 +1,3 @@
-StartDir = fullfile(pwd, '..', '..');
-cd (StartDir);
-
 SubLs = {
          'sub-02'
          'sub-03'
@@ -15,26 +12,12 @@ SubLs = {
 
 NbSub = numel(SubLs);
 
-FigureFolder = fullfile(StartDir, 'figures', 'behavioral');
-mkdir(FigureFolder);
-
-COLOR_Subject = [
-                 31, 120, 180
-                 178, 223, 138
-                 51, 160, 44
-                 251, 154, 153
-                 227, 26, 28
-                 253, 191, 111
-                 255, 127, 0
-                 202, 178, 214
-                 106, 61, 154
-                 0, 0, 130];
-COLOR_Subject = COLOR_Subject / 255;
+COLOR_SUBJECTS = SubjectColours();
 
 figure(1);
 hold on;
 for iSubj = 1:NbSub
-    plot([0 1], [iSubj iSubj], 'color', COLOR_Subject(iSubj, :), 'linewidth', 3);
+    plot([0 1], [iSubj iSubj], 'color', COLOR_SUBJECTS(iSubj, :), 'linewidth', 3);
 end
 axis([-.5 1.5 0  NbSub + 1]);
 axis off;
