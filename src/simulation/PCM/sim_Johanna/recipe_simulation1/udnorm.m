8c1c03b5ae0578dfdf49b2b2fad813f193c3758b
Remi Gau <remi_gau@hotmail.com>
2020-12-09 22:32:52 +0100

    rename and remove old functions

--- src/simulation/PCM/sim_Johanna/recipe_simulation1/udnorm.m
+++
@@ -1,8 +0,0 @@
-function [rdm_norm] = udnorm(rdm)
-    % function [ rdm_norm ] = udnorm(rdm)
-    % Takes norm of upper diagonal elements
-    % Johanna Zumer, 2017
-
-    rdm_norm = squareform(squareform(rdm) / norm(squareform(rdm)));
-
-end
