b4d47b30956b528a067c9873648d841e72976493
Remi Gau <remi_gau@hotmail.com>
2020-12-13 17:28:19 +0100

    refactor doing individual PCM

--- src/pcm/RunAllPcm.m
+++ src/pcm/RunAllPcm.m
@@ -2,20 +2,6 @@
 %
 % Runs the PCM
 
-% 3X3 models
-% on the 3 sensory modalities (A, V and T) but separately for
-% ipsi and contra
-%
-% It has 12 models that represent all the different ways that those 3
-% conditions can be either:
-%
-% - scaled
-% - scaled and independent
-% - independent
-%
-% See also `Set3X3models()`
-%
-
 % TODO
 % - Make it run on the b parameters
 % - Make it run on volume
@@ -27,7 +13,7 @@ close all;
 
 %% Main parameters
 
-ModelType = '6X6';
+ModelType = '3X3';
 
 % Choose on what type of data the analysis will be run
 %
@@ -46,11 +32,13 @@ InputType = 'Cst';
 %  possible choices: A1, PT, V1-5
 
 ROIs = { ...
-        'V1'
-        'V2'
-        'A1'
-        'PT'
-       };
+    'V1'
+    'V2'
+    'A1'
+    'PT'
+    };
+
+ROIs = {'A1'};
 
 %% Other parameters
 % Unlikely to change
@@ -62,6 +50,8 @@ DoFeaturePooling = true;
 Space = 'surf';
 
 MVNN = true;
+
+IndividualPcmDo = false;
 
 %%
 
@@ -83,31 +73,31 @@ end
 
 [SubLs, NbSub] = GetSubjectList(InputDir);
 
-FigureDir = fullfile(Dirs.PCM, ModelType, 'figures');
-mkdir(FigureDir);
+OutputDir = fullfile(Dirs.PCM, ModelType);
+mkdir(OutputDir);
 
 %% Build the models
 fprintf('Building models\n');
 
 switch lower(ModelType)
     case '3x3'
-
+        
         Models = Set3X3models();
-
+        
         %% Analysis name condition to use for it
-
+        
         Analysis(1).name = 'Ipsi';
         Analysis(1).CdtToSelect = 1:2:5;
-
+        
         Analysis(2).name = 'Contra';
         Analysis(2).CdtToSelect = 2:2:6;
-
+        
         Analysis(3).name = 'ContraIpsi';
         Analysis(3).CdtToSelect = 1:6;
-
+        
     case '6x6'
         Models = Set6X6models();
-
+        
         Analysis(1).name = 'AllConditions';
         Analysis(1).CdtToSelect = 1:6;
 end
@@ -116,51 +106,51 @@ end
 fprintf('Get started\n');
 
 for iROI =  1:numel(ROIs)
-
+    
     fprintf('\n %s\n', ROIs{iROI});
-
+    
     GrpData = {};
     GrpConditionVec = {};
     GrpRunVec = {};
-
+    
     clear G_hat G Gm COORD;
-
+    
     for ihs = 1:2
-
+        
         HsSufix = 'l';
         if ihs == 2
             HsSufix = 'r';
         end
-
+        
         fprintf('\n  Hemisphere: %s\n', HsSufix);
-
+        
         for iSub = 1:NbSub
-
+            
             fprintf('   Loading %s\n', SubLs(iSub).name);
-
+            
             SubDir = fullfile(InputDir, SubLs(iSub).name);
-
+            
             Filename = GetNameFileToLoad( ...
-                                         SubDir, SubLs(iSub).name, ...
-                                         HsSufix, ...
-                                         NbLayers, ...
-                                         ROIs{iROI}, ...
-                                         InputType);
-
+                SubDir, SubLs(iSub).name, ...
+                HsSufix, ...
+                NbLayers, ...
+                ROIs{iROI}, ...
+                InputType);
+            
             load(Filename, 'RoiData', 'ConditionVec', 'RunVec');
             LayerVec = ones(size(ConditionVec));
             if strcmp(InputType, 'ROI')
                 load(Filename, 'LayerVec');
             end
-
+            
             [RoiData, RunVec, ConditionVec, LayerVec] = CheckInput(RoiData, ...
-                                                                   RunVec, ...
-                                                                   ConditionVec, ...
-                                                                   IsTarget, ...
-                                                                   LayerVec);
-
+                RunVec, ...
+                ConditionVec, ...
+                IsTarget, ...
+                LayerVec);
+            
             RoiData = ReassignIpsiAndContra(RoiData, ConditionVec, HsSufix, DoFeaturePooling);
-
+            
             % If we have the layers data on several rows of the data
             % matrix we put them back on a single row
             CvMat = [ConditionVec RunVec LayerVec];
@@ -169,23 +159,23 @@ for iROI =  1:numel(ROIs)
             end
             ConditionVec = CvMat(:, 1);
             RunVec = CvMat(:, 2);
-
+            
             GrpData{iSub, ihs} = RoiData; %#ok<*SAGROW>
             GrpConditionVec{iSub} = ConditionVec;
             GrpRunVec{iSub} = RunVec;
-
+            
         end
-
+        
     end
-
+    
     %% Pool data between hemispheres
     tmp = {};
     for iSub = 1:NbSub
         tmp{iSub, 1} = [GrpData{iSub, 1} GrpData{iSub, 2}];
     end
-
+    
     GrpData = tmp;
-
+    
     switch lower(ModelType)
         case '3x3'
             Models = Set3X3models();
@@ -196,70 +186,86 @@ for iROI =  1:numel(ROIs)
             end
             Models = Set6X6models(AuditoryOrVisual);
     end
-
+    
     %% Run the PCM
-
+    
     GrpDataSource = GrpData;
     GrpConditionVecSource = GrpConditionVec;
     GrpRunVecSource = GrpRunVec;
-
+    
     for iAnalysis = 1:numel(Analysis)
-
-        fprintf('\n\n  Running analysis: %s\n\n', Analysis(iAnalysis).name);
-
+        
+        fprintf('\n\n  Running analysis: %s\n', Analysis(iAnalysis).name);
+        
+        Filename = ['pcm_results', ...
+            '_roi-', ROIs{iROI}, ...
+            '_cdt-', ConditionType, ...
+            '_param-', lower(InputType), ...
+            '_analysis-', Analysis(iAnalysis).name, ...
+            '.mat'];
+        
         [GrpData, GrpRunVec, GrpConditionVec] = PreparePcmInput( ...
-                                                                GrpDataSource, ...
-                                                                GrpConditionVecSource, ...
-                                                                GrpRunVecSource, ...
-                                                                Analysis(iAnalysis));
-
+            GrpDataSource, ...
+            GrpConditionVecSource, ...
+            GrpRunVecSource, ...
+            Analysis(iAnalysis));
+        
         G_hat = ComputeGmatrix(GrpData, GrpRunVec, GrpConditionVec);
-
-        [T_grp, theta_grp, G_pred_grp, T_cr, theta_cr, G_pred_cr] = RunPcm( ...
-                                                                           GrpData, ...
-                                                                           Models, ...
-                                                                           GrpRunVec, ...
-                                                                           GrpConditionVec);
-
-        % Save
-        filename = ['pcm_results', ...
-                    '_roi-', ROIs{iROI}, ...
-                    '_cdt-', ConditionType, ...
-                    '_param-', lower(InputType), ...
-                    '_analysis-', Analysis(iAnalysis).name, ...
-                    '.mat'];
-        filename = fullfile(Dirs.PCM, ModelType, filename);
-
-        save(filename, ...
-             'Analysis', ...
-             'Models', ...
-             'GrpRunVec', 'GrpConditionVec', ...
-             'G_hat', ...
-             'T_grp', 'theta_grp', 'G_pred_grp', ...
-             'T_cr',  'theta_cr',  'G_pred_cr');
-
-    end
-
-end
-
-function varargout = PreparePcmInput(Data, RunVec, ConditionVec, Analysis)
-
-    for iSub = 1:size(Data, 1)
-
-        % Only keep the conditions for that analysis
-
-        ConditionVec{iSub}(~ismember(ConditionVec{iSub}, Analysis.CdtToSelect)) = 0;
-
-        if strcmpi(Analysis.name, 'contraipsi')
-            [Data{iSub}, ConditionVec{iSub}, RunVec{iSub}] = CombineIpsiAndContra( ...
-                                                                                  Data{iSub}, ...
-                                                                                  ConditionVec{iSub}, ...
-                                                                                  RunVec{iSub}, ...
-                                                                                  'pool');
+        
+        if IndividualPcmDo
+            [T_ind, theta_ind, G_pred_ind, D, T_ind_cross, theta_ind_cross] = RunIndividualPcm(...
+            GrpData, ...
+            Models, ...
+            GrpRunVec, ...
+            GrpConditionVec);
+            
+            save(fullfile(OutputDir,  ['individual_' Filename]), ...
+                'Models', ...
+                'T_ind', 'theta_ind', 'G_pred_ind', ...
+                'D', 'T_ind_cross', 'theta_ind_cross');
         end
 
+        [T_grp, theta_grp, G_pred_grp, T_cr, theta_cr, G_pred_cr] = RunGroupPcm( ...
+            GrpData, ...
+            Models, ...
+            GrpRunVec, ...
+            GrpConditionVec);
+        
+        save(fullfile(OutputDir, ['group_' Filename]), ...
+            'Analysis', ...
+            'Models', ...
+            'GrpRunVec', 'GrpConditionVec', ...
+            'G_hat', ...
+            'T_grp', 'theta_grp', 'G_pred_grp', ...
+            'T_cr',  'theta_cr',  'G_pred_cr');
+        
     end
+    
+end
 
-    varargout = {Data, RunVec, ConditionVec};
 
+
+function [T_ind, theta_ind, G_pred_ind, D, T_ind_cross, theta_ind_cross] = RunIndividualPcm(Data, Models, RunVec, ConditionVec)
+    
+    MaxIteration = 50000;
+    runEffect  = 'fixed';
+    
+    fprintf('   Doing individual analysis\n');
+    
+    [T_ind, theta_ind, G_pred_ind] = pcm_fitModelIndivid(...
+        Data, ...
+        Models, ...
+        RunVec, ...
+        ConditionVec, ...
+        'runEffect', runEffect, ...
+        'MaxIteration', MaxIteration); %#ok<*ASGLU>
+    
+    [D, T_ind_cross, theta_ind_cross] = pcm_fitModelIndividCrossval(...
+        Data, ...
+        Models, ...
+        RunVec, ...
+        ConditionVec, ...
+        'runEffect', runEffect, ...
+        'MaxIteration', MaxIteration);
+    
 end
---
+++ src/rsa/ComputeRSA.m
@@ -0,0 +1,35 @@
+
+
+% compute RSA distances (A --> B) in a cross validated fashion
+A =  rsa.distanceLDC(Y{iSub}(:, Vert2Take), partVec{iSub}, condVec{iSub});
+
+% because when doing cross validation distance A --> B can be different
+% from B-->A we recompute the RSA in the other direction and take the
+% mean of both directions
+
+% compute RSA distances (B --> A) in a cross validated fashion
+% flipup the data and the partition vector means that condition label
+% used to be 1:6 is now 6:-1:1
+B =  rsa.distanceLDC(flipud(Y{iSub}(:, Vert2Take)), flipud(partVec{iSub}), condVec{iSub});
+% flip the distance back
+B = fliplr(B);
+B = [B(1:2) B(4) B(7) B(11) B(3) B(5) B(8) B(12) B(6) B(9) B(13) B(10) B(14:15)];
+% take the mean
+RDMs_CV(:, :, iSub, iSplit) = squareform(mean([A; B]));
+
+%% Compute CVed G-matrix, do multidimensional scaling
+G_hat(:, :, iSub, iSplit) = pcm_estGCrossval(Y{iSub}(:, Vert2Take), partVec{iSub}, condVec{iSub});
+
+
+
+%%
+% Eucledian normalization
+for i = 1:size(X, 1)
+    X(i, :) = X(i, :) / norm(X(i, :));
+end
+clear i;
+
+A =  rsa.distanceLDC(X, partitionVec, conditionVec);
+B =  rsa.distanceLDC(flipud(X), flipud(partitionVec), conditionVec);
+B = fliplr(B);
+RDMs{iROI, iHS, iTarget + 1} = squareform(mean([A; B]));
