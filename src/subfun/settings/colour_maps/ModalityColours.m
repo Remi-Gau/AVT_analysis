6b5172f06f01d66a5e000536a856b20cfeebb144
Remi Gau <remi_gau@hotmail.com>
2021-02-03 17:05:22 +0100

    move color map functions

---
+++ src/subfun/settings/colour_maps/ModalityColours.m
@@ -0,0 +1,12 @@
+% (C) Copyright 2021 Remi Gau
+
+function COLOR_MODALITIES = ModalityColours()
+
+    COLOR_MODALITIES = [
+                        27, 158, 119; ...
+                        217, 95, 2; ...
+                        117, 112, 179 ...
+                       ];
+    COLOR_MODALITIES = COLOR_MODALITIES / 255;
+
+end
