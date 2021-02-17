ab0580b4e00e75b6839a607925254cd6ab18e0d1
Remi Gau <remi_gau@hotmail.com>
2021-02-17 16:47:26 +0100

    plot subjects for profile difference

--- src/settings/plotting/SetProfilePlottingOptions.m
+++ src/settings/plotting/SetProfilePlottingOptions.m
@@ -26,7 +26,7 @@ function Opt = SetProfilePlottingOptions(Opt)
         end
         Opt.Specific{1, i}.IsMvpa = false;
         Opt.Specific{1, i}.Ttest.SideOfTtest = 'both';
-        Opt.Specific{1, i}.PlotSubjects = false;
+        Opt.Specific{1, i}.PlotSubjects = true;
     end
 
     if isfield(Opt, 'IsDifferencePlot') && Opt.IsDifferencePlot
@@ -40,7 +40,7 @@ function Opt = SetProfilePlottingOptions(Opt)
         Opt.Specific{1, 2}.BetaSubplot = {[16 18]; [20 22]; [24 26]};
         Opt.Specific{1, 2}.LineColors = [127 127 127] / 256;
 
-        Opt.Specific{1, 2}.PlotMinMaxType = 'group';
+        Opt.Specific{1, 2}.PlotMinMaxType = 'all';
     end
 
 end
