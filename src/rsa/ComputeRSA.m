a081f75cffd1712bffcfcecd52d44618d2f43b4b
Remi Gau <remi_gau@hotmail.com>
2021-08-03 18:16:02 +0200

    refactor RSA set up

--- src/rsa/ComputeRSA.m
+++ src/rsa/ComputeRSA.m
@@ -1,4 +1,4 @@
-% (C) Copyright 2020 Remi Gau
+% (C) Copyright 2021 Remi Gau
 %
 % Runs RSA computation
 
@@ -11,57 +11,8 @@ clc;
 clear;
 close all;
 
-%% Main parameters
+[InputType, ROIs, Opt, ConditionType, Analysis, CondNames, Dirs] = SetRsa;
 
-% Choose on what type of data the analysis will be run
-%
-% b-parameters
-%
-% 'ROI'
-%
-% s-parameters
-%
-% 'Cst', 'Lin', 'Quad'
-%
-
-InputType = 'Cst';
-
-% Region of interest:
-%  possible choices: A1, PT, V1-5
-
-ROIs = { ...
-        'V1'
-        'V2'
-        'A1'
-        'PT'
-       };
-
-%% Other parameters
-
-Opt = SetDefaults();
-
-Opt.CombineHemisphere = true;
-
-Space = 'surf';
-MVNN = true;
-
-%%
-ConditionType = 'stim';
-Analysis.name = 'all_stim';
-Analysis.CdtToSelect = 1:6;
-
-if Opt.Targets
-    ConditionType = 'target'; %#ok<*UNRCH>
-    Analysis.CdtToSelect = 7:12;
-    Analysis.name = 'all_target';
-end
-
-CondNames = GetConditionList();
-CondNames = CondNames(Analysis.CdtToSelect);
-
-% TODO
-% This input dir might have to change if we are dealing with volume data
-Dirs = SetDir(Space, MVNN);
 InputDir = Dirs.ExtractedBetas;
 if any(ismember(InputType, {'Cst', 'Lin', 'Quad'}))
     InputDir = Dirs.LaminarGlm;
